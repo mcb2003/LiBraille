a5b50cf0e88e541539e5457022b7b16b1c8e4c77
mcb2003 <mikeybuchan@hotmail.co.uk>
2020-12-16 14:20:37 +0000

    Create simple menu and status bars.

--- ui/__init__.py
+++ ui/__init__.py
@@ -4,8 +4,21 @@ class MainFrame(wx.Frame):
     """ The application's main window """
     def __init__(self, parent = None, *args, **kwargs):
         super().__init__(parent, title="Libraille", size=(800, 600))
+        self.CreateStatusBar()
+        # Create and attach the MenuBar
+        self.menu_bar = self.createMenuBar()
+        self.SetMenuBar(self.menu_bar)
         self.text = wx.TextCtrl(self, style=wx.TE_MULTILINE)
         self.Show(True)
+
+    def createMenuBar(self):
+        file = wx.Menu()
+        file.Append(wx.ID_OPEN, "&Import", "Import and convert a print document")
+        file.AppendSeparator()
+        file.Append(wx.ID_EXIT, "E&xit", "Close this program")
+        menu = wx.MenuBar()
+        menu.Append(file, "&File")
+        return menu
 
 class App(wx.App):
     """ Represents the  entire GUI application """
